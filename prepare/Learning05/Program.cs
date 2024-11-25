10838637257c0974244d1e347008b33a61a8fdb4
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2024-11-25 12:14:56 +0200

    Learning Activity05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -3,7 +3,21 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Learning05 World!");
+      Console.WriteLine("Hello Learning05 World!");  
+    }
+    {
+        
+        Assignment a1 = new Assignment("Tsitsi Mutsvedu", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Ronald Garry", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
